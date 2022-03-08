d7f4221af51e6937ba2f2dbcaaa48f2e6db743ca
Zeke Wessell <wessellz@ledyard.net>
2022-03-08 14:03:20 -0500

    added the list formating function, the dice rolling function, and a mainloop to the main file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from setuptools import setup
 import MTG_Life_Counter_Functions as mtg
 pl_index = 3
 player_num = mtg.startup()
-life = mtg.setup(player_num)
 players = mtg.player_names(player_num)
-mtg.request_query(players, life)
+life = mtg.setup(player_num)
+running = True
+while running == True:
+    mtg.request_query(players, life)
+    for i in range(len(players)):
+        print(f'{mtg.lf(players[i])} has {mtg.lf(life[i])} life ')
