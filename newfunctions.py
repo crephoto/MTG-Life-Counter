990b87f83e83f53752bfb960b367e5b0e095ea46
Roan Fothergill <fothergillr@ledyard.net>
2022-03-30 13:57:36 -0400

    added more user ability to the gui

--- newfunctions.py
+++ newfunctions.py
@@ -1,19 +1,21 @@
+from cgi import test
+from tkinter import *
+
+def playernamewindow():
+    pass
+
 # This gets the number of players 
-from numpy import true_divide
-
-
 def startup():
-    players = 0
-    while players<2 or players>6:
-        players = input("How many people are playing (max6) ")
+    advance = False
+    while advance == False:
+        players = input("How many people are playing? ")
         try:
             players = int(players)
         except ValueError:
             print("That isn't an Integer, Try again")
             players = 0
             continue
-        if players>6 or players<2:
-            print("Too many or too little players, Try again")
+        advance = True
     return(players)
 
 # This takes the number of players and returns a list with starting life, this will correspond to a player in the Players list
@@ -25,10 +27,10 @@ def setup(player_num):
         try:
             life_count = int(life_count)
         except ValueError:
-            print("That isn't a whole number, Try again")
+            print("That isn't a natural number, Try again")
             continue
-        if life_count < 0:
-            print("That isn't a whole number, Try again")
+        if life_count <= 0:
+            print("That isn't a natural number, Try again")
             continue
         advance = True
     while player_num > 0:
@@ -39,8 +41,8 @@ def setup(player_num):
 # This returns a list of player names, the names will correspond to a life total in the Life list    
 def player_names(player_num):    
         players = []
-        for i in player_num:
-            players.append(input(f'What is the name of player {i}? '))
+        for i in range(player_num):
+            players.append(input(f'What is the name of player {i+1}? '))
         return(players)
 
 # This takes a player's life and changes it by a user-determined ammount
@@ -58,7 +60,7 @@ def life_change(player, life):
     if life <= 0:
         print(f'{player} has died')
     else:
-        print(f'{lf(player, False, 0)} now has {lf(str(life, False, 0))} life')
+        print(f'{lf(player, False, 0)} now has {lf(str(life), False, 0)} life')
     return(life)
 
 # This will allow a user to activate other functions
@@ -73,7 +75,7 @@ def request_query(players, Life, player_num):
         else:
             advance = True
     if action == 1:
-        player_index = input(f'Whose life should be changed? {lf(players)} ')
+        player_index = input(f'Whose life should be changed? {lf(players, False, 0)} ')
         player_index = players.index(player_index)
         Life[player_index] = life_change(players[player_index], Life[player_index])
     if action == 2:
@@ -125,6 +127,7 @@ def lf(list, cd, player_num):
         list = list.replace("'", "")
     return(list)
 
+# This stores a list filled with a list that has numbers corresponding to each players dammage from a certain other player.
 def commander_dmg(dammage, player_num):
     advance = False
     while advance == False:
@@ -135,11 +138,66 @@ def commander_dmg(dammage, player_num):
             advance = True
     if decide == "1":
         if dammage == []:
-            dammage = [[0]*player_num-1]*player_num
+            dammage = [[0]*(player_num-1)]*player_num
             print(lf(dammage, True, player_num))
 
             
 
+def main(player_num):
+    players = player_names(player_num)
+    life = setup(player_num)
+    running = True
+    while running == True:
+        request_query(players, life, player_num)
+        for i in range(len(players)):
+            print(f'{(players[i], False, player_num)} has {lf(life[i], False, player_num)} life ')
+                
 
-            
+    
+screen  = Tk()
+#creating text
+mylabel = Label(screen, text= "Welcome! Choose how many players are playing.")
+screen.title("The best MTG App!")
+#screen size
+screen.geometry("600x600")
+#creates button 
+btn_2 = Button(screen, text = '2', command=lambda m = 2 : button_press(m))
+btn_3 = Button(screen, text = '3', command=lambda m = 3 : button_press(m))
+btn_4 = Button(screen, text = '4', command=lambda m = 4 : button_press(m))
+btn_5 = Button(screen, text = '5', command=lambda m = 5 : button_press(m))
+btn_6 = Button(screen, text = '6', command=lambda m = 6 : button_press(m))
+#Set Grid
+mylabel.grid(row=0, column=0,sticky="NSEW")
+btn_2.grid(row=1, column=0,sticky="NSEW")
+btn_3.grid(row=2, column=0,sticky="NSEW")
+btn_4.grid(row=3, column=0,sticky="NSEW")
+btn_5.grid(row=4, column=0,sticky="NSEW")
+btn_6.grid(row=5, column=0,sticky="NSEW")
+#configue our column and rows
+row_number = 1
+Grid.columnconfigure(screen, 0, weight=1)
+button_list = [btn_2, btn_3, btn_4, btn_5, btn_6]
+for button in button_list:
+    Grid.rowconfigure(screen, row_number, weight=1)
+    row_number+=1
 
+def button_press(which_one):
+    import tkinter as tk
+    player_num = which_one
+    clear_frame()
+    
+    
+    for i in range(player_num):
+        text = tk.Text(screen, height=5, width=10)
+        text.config(state="normal")
+        text.pack()
+        players = [text]
+        print(players)
+        
+
+def clear_frame():
+    for widgets in screen.winfo_children():
+        widgets.destroy()
+
+
+screen.mainloop()
